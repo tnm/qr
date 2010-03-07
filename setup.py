1ae2c12ee62e07da2396ef2221f6e090ae672cc6
Ted Nyman <tednyman@ted-nymans-mac-pro.local>
2010-03-07 15:30:38 -0800

    updates to setup.py

--- setup.py
+++ setup.py
@@ -30,12 +30,19 @@ class TestCommand(Command):
 
 LONG_DESCRIPTION = '''
 
+Version 0.1.4 Updates:
+----------------------
+
+Auto-pop functionality is now optional. New tests are now included. Additional docstrings and some other minor changes. 
+
 QR
 --
 
 QR helps you create and work with deque, queue, and stack data structures for Redis. 
 
 Redis is well-suited for implementations of these abstract data structures, and QR makes the work even easier in Python. QR works best for (and simplifies) the creation of bounded deques, queues, and stacks (herein, DQS's), with a defined size of elements.
+
+This version is designed for best usage in single-writer environments. Version 0.2 will be designed for full usage in multiple-writer environments as well.
 
 Quick Setup
 -----------
@@ -44,7 +51,7 @@ You'll need Redis itself, and the current Python interface for Redis, redis-py. 
 
 qr.py also creates an instance of the redis-py interface object. You may already have instantiated the object in your code, so you'll want to ensure consistent namespacing. You can remove this line of code, modify the namespacing, or adjust your existing namespacing -- whatever works best for you.
 
-Full documentation is in README.md, or at http://github.com/tnm/qr
+Full documentation is at http://github.com/tnm/qr
 
 '''
 
@@ -52,7 +59,7 @@ Full documentation is in README.md, or at http://github.com/tnm/qr
 setup(
     name='qr',
     version=version,
-    description='Create and interact with Redis-based data structures in Python',
+    description='Create and work with Redis-backed queues, deques, stacks -- bounded and unbounded',
     long_description=LONG_DESCRIPTION,
     url='http://github.com/tnm/qr',
     author='Ted Nyman',
