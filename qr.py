3a16da68ef7ae5a214567bd8c76002ee877f9331
Dan Lecocq <dan@seomoz.org>
2011-10-03 11:51:51 -0700

    Fixed extend, and popping.

--- qr.py
+++ qr.py
@@ -23,9 +23,6 @@ try:
 except ImportError:
     import pickle
 
-# redis interface
-redis = redis.Redis()
-
 class NullHandler(logging.Handler):
     """A logging handler that discards all logging records"""
     def emit(self, record):
@@ -36,20 +33,21 @@ log = logging.getLogger('qr')
 log.addHandler(NullHandler())
 
 class BaseQueue(object):
-    def __init__(self, key):
+    def __init__(self, key, **kwargs):
         self.serializer = pickle
+        self.redis = redis.Redis(**kwargs)
         self.key = key
     
     def __len__(self):
         """Return the length of the queue"""
-        return redis.llen(self.key)
+        return self.redis.llen(self.key)
     
     def __getitem__(self, val):
         """Get a slice or a particular index."""
         try:
-            return redis.lrange(self.key, val.start, val.stop)
+            return self.redis.lrange(self.key, val.start, val.stop)
         except AttributeError:
-            return redis.lindex(self.key, val)
+            return self.redis.lindex(self.key, val)
         except Exception as e:
             log.error('Get item failed ** %s' % repr(e))
             return []
@@ -60,12 +58,17 @@ class BaseQueue(object):
     
     def _unpack(self, val):
         """Unpacks a message stored in Redis"""
-        return self.serializer.loads(val)
+        try:
+            return self.serializer.loads(val)
+        except TypeError:
+            return None
 
-    def extend(self, *vals):
+    def extend(self, vals):
         """Extends the elements in the queue."""
+        pipe = self.redis.pipeline()
         for val in vals:
-            redis.lpush(self._pack(val))
+            self.redis.lpush(self.key, self._pack(val))
+        pipe.execute()
 
     def peek(self):
         """Look at the next item in the queue"""
@@ -73,11 +76,11 @@ class BaseQueue(object):
 
     def elements(self):
         """Return all elements as a Python list"""
-        return redis.lrange(self.key, 0, -1)
+        return self.redis.lrange(self.key, 0, -1)
     
     def elements_as_json(self):
         """Return all elements as JSON object"""
-        all_elements = redis.lrange(self.key, 0, -1)
+        all_elements = self.redis.lrange(self.key, 0, -1)
         return json.dumps(all_elements)
 
 class Deque(BaseQueue):
@@ -85,24 +88,24 @@ class Deque(BaseQueue):
 
     def push_back(self, element):
         """Push an element to the back of the deque"""
-        redis.lpush(self.key, self._pack(element))
+        self.redis.lpush(self.key, self._pack(element))
         log.debug('Pushed ** %s ** for key ** %s **' % (element, self.key))
         
     def push_front(self, element):
         """Push an element to the front of the deque"""
         key = self.key
-        push_it = redis.rpush(key, self._pack(element))
+        push_it = self.redis.rpush(key, self._pack(element))
         log.debug('Pushed ** %s ** for key ** %s **' % (element, self.key))
 
     def pop_front(self):
         """Pop an element from the front of the deque"""
-        popped = redis.rpop(self.key)
+        popped = self.redis.rpop(self.key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, self.key))
         return self._unpack(popped )
 
     def pop_back(self):
         """Pop an element from the back of the deque"""
-        popped = redis.lpop(self.key)
+        popped = self.redis.lpop(self.key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, self.key))
         return self._unpack(popped)
 
@@ -111,12 +114,12 @@ class Queue(BaseQueue):
     
     def push(self, element):
         """Push an element"""
-        redis.lpush(self.key, self._pack(element))
+        self.redis.lpush(self.key, self._pack(element))
         log.debug('Pushed ** %s ** for key ** %s **' % (element, self.key))
 
     def pop(self):
         """Pop an element"""
-        popped = redis.rpop(self.key)
+        popped = self.redis.rpop(self.key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, self.key))
         return self._unpack(popped)
     
@@ -132,12 +135,20 @@ class CappedCollection(BaseQueue):
 
     def push(self, element):
         size = self.size
-        pipe = redis.pipeline() # Use multi-exec command via redis-py pipelining
+        pipe = self.redis.pipeline() # Use multi-exec command via redis-py pipelining
         pipe = pipe.lpush(self.key, self._pack(element)).ltrim(self.key, 0, size-1) # ltrim is zero-indexed 
         pipe.execute()
 
+    def extend(self, vals):
+        """Extends the elements in the queue."""
+        pipe = self.redis.pipeline()
+        for val in vals:
+            self.redis.lpush(self.key, self._pack(val))
+        pipe.ltrim(self.key, 0, self.size-1)
+        pipe.execute()
+
     def pop(self):
-        popped = redis.rpop(self.key)
+        popped = self.redis.rpop(self.key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, self.key))
         return self._unpack(popped)
 
@@ -146,12 +157,12 @@ class Stack(BaseQueue):
 
     def push(self, element):
         """Push an element"""
-        redis.lpush(self.key, self._pack(element))
+        self.redis.lpush(self.key, self._pack(element))
         log.debug('Pushed ** %s ** for key ** %s **' % (element, self.key))
          
     def pop(self):
         """Pop an element"""
-        popped = redis.lpop(self.key)
+        popped = self.redis.lpop(self.key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, self.key))
         return self._unpack(popped)
     
